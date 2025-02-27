48b9d889b1ed3e2a1cbdad00b97ba39989cd0cc0
gksdyd <gksdyd009@naver.com>
2025-02-27 15:42:38 +0900

    1. git init

---
+++ nexera/src/main/java/com/nexera/demo/NexeraApplication.java
@@ -0,0 +1,13 @@
+package com.nexera.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexeraApplication.class, args);
+	}
+
+}
